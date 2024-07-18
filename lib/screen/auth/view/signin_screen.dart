87102f3aaa105e9fd9e38c79ac878e286d78c8f6
r79 <ajaypurohit992@gmail.com>
2024-07-18 19:02:18 +0530

    "new"

--- lib/screen/auth/view/signin_screen.dart
+++ lib/screen/auth/view/signin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
@@ -8,8 +9,126 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Image.asset(
+                    "assets/logo/Login-pana (1).png",
+                    height: 300,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        fillColor: Colors.grey,
+                        labelText: "Email"),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        fillColor: Colors.grey,
+                        labelText: "Password"),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 50,
+                      width: MediaQuery.sizeOf(context).width,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff084759),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(),
+                      ),
+                      child: const Text(
+                        "Sign in",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const Divider(
+                    color: Colors.black,
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/logo/google.png",
+                        fit: BoxFit.cover,
+                        height: 50,
+                      ),
+                      Image.asset(
+                        "assets/logo/facebook.png",
+                        fit: BoxFit.cover,
+                        height: 50,
+                      ),
+                      Image.asset(
+                        "assets/logo/a.png",
+                        fit: BoxFit.cover,
+                        height: 50,
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  InkWell(
+                    onTap: () {},
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Don't have an account?",
+                            style: TextStyle(color: Colors.black, fontSize: 18),
+                          ),
+                          TextSpan(
+                            text: " Create now",
+                            style: TextStyle(
+                                color: Color(0xff084759),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
